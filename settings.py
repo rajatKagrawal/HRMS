85bbab29a0605a8acdde0da4edb9a3d2a61c017f
Rajat.KAgrawal <rajat.kagrawal1@delhivery.com>
2019-12-14 13:43:52 +0530

    adding settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/hrms'
+db = SQLAlchemy(app)
+db.create_all()
+db.session.commit()
